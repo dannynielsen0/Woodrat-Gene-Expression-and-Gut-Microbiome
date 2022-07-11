c6784e882e2309a27fcf4d16d25c283edf4876aa
dannynielsen0 <89220441+dannynielsen0@users.noreply.github.com>
2022-07-11 14:56:21 -0700

    updates to code

--- code/feeding_trial_RRA_barcharts.R
+++ code/feeding_trial_RRA_barcharts.R
@@ -25,38 +25,41 @@ physeq1@sam_data$sp_diet <- paste(physeq1@sam_data$Species, physeq1@sam_data$Die
 
 ########To plot the desired taxanomic level, change each level between this line and the next string of #s below
 
-y1 <- tax_glom(physeq1, taxrank = 'Family') # agglomerate taxa at desired level
-y2 = merge_samples(y1, "sp_diet") # merge samples on sample variable of interest
+y1 <- tax_glom(physeq1, taxrank = 'Phylum') # agglomerate taxa at desired level
+y2 = suppressWarnings(merge_samples(y1, "sp_diet")) # merge samples on sample variable of interest; suppress warnings is to prevent from failing
 y3 <- transform_sample_counts(y2, function(x) x/sum(x)) #get abundance in %
-y4 <- psmelt(y3) # create dataframe from phyloseq object
-y4$Family <- as.character(y4$Family) #convert to character
-y4$Family[y4$Abundance < 0.05] <- "Families < 5% abund." #rename genera with < .25% abundance
+y4 <- suppressWarnings(psmelt(y3)) # create dataframe from phyloseq object
+y4$Phylum <- as.character(y4$Phylum) #convert to character
+y4$Phylum[y4$Abundance < 0.05] <- "Phyla < 5% abund." #rename genera with < .25% abundance
 
 #split back out the location and staph into unique columns
 y4 <- cbind(y4, read.table(text=y4$Sample, sep="_", header=FALSE, col.names = paste0("col", 1:2), stringsAsFactors=FALSE))
-y4$Family <- factor(y4$Family, levels=rev(unique(y4$Family)))
+y4$Phylum <- factor(y4$Phylum, levels=rev(unique(y4$Phylum)))
 
 #change order so lepida on left
 y4$col1 <- factor(y4$col1, levels = c("N. lepida", "N. bryanti"))
 
 
 #set color palette to accommodate the number of genera
-mycolors <- colorRampPalette(brewer.pal(8, "Paired"))(length(unique(y4$Family)))
+mycolors <- colorRampPalette(brewer.pal(8, "Paired"))(length(unique(y4$Phylum)))
 
 
 #plot
-p <- ggplot(data=y4, aes(x=col2, y=Abundance, fill=Family, Genus =Family)) +
+p <- ggplot(data=y4, aes(x=col2, y=Abundance, fill=Phylum, Genus =Phylum)) +
   geom_bar(aes(), stat="identity", position="stack") + scale_fill_manual(values=mycolors) +
   theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),panel.background = element_blank(), axis.line = element_line(colour = "black")) +
-  theme(legend.position="bottom", legend.title=element_text(size=18), legend.text=element_text(size=18)) + guides(fill=guide_legend(nrow=5)) + 
+  theme(legend.position="bottom", legend.title=element_text(size=20), legend.text=element_text(size=20)) + guides(fill=guide_legend(nrow=5)) + 
   scale_x_discrete(limits=rev(levels(as.factor(y4$col2)))) + facet_wrap(~col1) +
+  theme(axis.text.x = element_text(size = 20)) +
+  theme(axis.text.y = element_text(size = 20),
+        axis.title=element_text(size=20,face="bold")) + theme(strip.text.x = element_text(size = 24)) +
   guides(fill = guide_legend(reverse = TRUE)) +
   ylab("16S relative read abundance") + xlab("")
 p
 
 
 #Genus
-ggsave(plot=p, "../Lab-diet-trial-16S-analysis/figures/Genus_RRA.jpg", width = 14, height =8 , device='jpg', dpi=500)
+ggsave(plot=p, "../Lab-diet-trial-16S-analysis/figures/phyla_RRA.jpg", width = 14, height =8 , device='jpg', dpi=500)
 ########
 
 
